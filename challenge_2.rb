0ff36d4122727f2297e5c97a44f2cbb75873d4a9
SebasNait15 <link.ibarra@gmail.com>
2024-09-02 13:01:43 -0600

    Challenge 2 Identify a prime number solved

--- challenge_2.rb
+++ challenge_2.rb
@@ -4,7 +4,7 @@
 # Identify from a array of numers how many are prime and show them. 
 # Solve by Sebastian Ibarra
 
-numbers=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+numbers=[]
 prime=[]
 continue = true
 element = 0
