60fc22565d30ed08d2c4973067c32a086cfc3b71
SebasNait15 <link.ibarra@gmail.com>
2024-09-06 09:27:31 -0600

    Challenge 4 calculate a factorial number solved

---
+++ factorial_number.rb
@@ -0,0 +1,36 @@
+require_relative 'functions'
+
+def factorial_number(number, result)
+    if number == 0 
+        return result
+    else 
+        result = result * number
+        number = number -1
+        factorial_number(number, result)
+    end 
+end
+
+def main 
+    number = -1
+    result = 1
+
+    while number < 0 
+    
+        #Ask and white a number
+        number = ask_for_integer("Insert a natural number: ")
+        
+        #Clean screan
+        system('cls')
+    
+        if number < 0 
+            puts "Number is not natural, Try again"
+        end  
+    
+    end
+    
+    result = factorial_number(number, result)
+    puts "The factorial of:  #{number} is : #{result}"
+
+end 
+
+main
